6972c0a6fc0431c7e41b110ea8c41dd9a4ed076c
Simon Pinfold <simon@uint8.me>
2016-01-18 21:54:01 +1300

    Add distutils setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+	name='python-fsb5',
+	version='1.0',
+	author='Simon Pinfold',
+	author_email='simon@uint8.me',
+	description='Library and to extract audio from FSB5 (FMOD Sample Bank) files',
+	download_url='https://github.com/synap5e/python-fsb5/tarball/master',
+	license='MIT',
+	url='https://github.com/synap5e/python-fsb5',
+)
