1bfab38d83d639600c6117f715d647809c357f16
Jerome Leclanche <jerome@leclan.ch>
2016-05-03 06:45:41 +0300

    Update setup.py style

--- setup.py
+++ setup.py
@@ -5,27 +5,27 @@ from setuptools import setup, find_packages
 
 
 CLASSIFIERS = [
-	'Development Status :: 4 - Beta',
-	'Intended Audience :: Developers',
-	'License :: OSI Approved :: MIT License',
-	'Programming Language :: Python',
-	'Programming Language :: Python :: 3',
-	'Programming Language :: Python :: 3.4',
-	'Programming Language :: Python :: 3.5',
-	'Topic :: Multimedia :: Sound/Audio',
-	'Topic :: Multimedia :: Sound/Audio :: Conversion',
+	"Development Status :: 4 - Beta",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: MIT License",
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.4",
+	"Programming Language :: Python :: 3.5",
+	"Topic :: Multimedia :: Sound/Audio",
+	"Topic :: Multimedia :: Sound/Audio :: Conversion",
 ]
 
 
 setup(
-	name='fsb5',
+	name="fsb5",
 	version=fsb5.__version__,
 	author=fsb5.__author__,
 	author_email=fsb5.__email__,
-	description='Library and to extract audio from FSB5 (FMOD Sample Bank) files',
-	download_url='https://github.com/HearthSim/python-fsb5/tarball/master',
-	license='MIT',
-	url='https://github.com/HearthSim/python-fsb5',
+	description="Library and to extract audio from FSB5 (FMOD Sample Bank) files",
+	download_url="https://github.com/HearthSim/python-fsb5/tarball/master",
+	license="MIT",
+	url="https://github.com/HearthSim/python-fsb5",
 	classifiers=CLASSIFIERS,
 	packages=find_packages(),
 )
