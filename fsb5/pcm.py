77b29ce5ee07e3385c9d2f5578a9b9ba532a5b77
Rémy F <yne@users.noreply.github.com>
2021-05-17 21:05:37 +0200

    Fix Stereo PCM Extraction
    Extracting Stereo PCM allocation need to use `sample.channels`.
    Otherwise, only Mono PCM works, or only the first half of the Stero PCM got written.

--- fsb5/pcm.py
+++ fsb5/pcm.py
@@ -3,7 +3,7 @@ from io import BytesIO
 
 
 def rebuild(sample, width):
-	data = sample.data[:sample.samples * width]
+	data = sample.data[:sample.samples * sample.channels * width]
 	ret = BytesIO()
 	with wave.open(ret, "wb") as wav:
 		wav.setparams((sample.channels, width, sample.frequency, 0, "NONE", "NONE"))
